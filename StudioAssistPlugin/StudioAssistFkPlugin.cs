01394a848b9694843b56e0d4b4101e23b1c40716
yuemenglong <yuemenglong@126.com>
2020-06-16 07:48:24 +0800

    add: IK/FK

--- StudioAssistPlugin/StudioAssistFkPlugin.cs
+++ StudioAssistPlugin/StudioAssistFkPlugin.cs
@@ -8,13 +8,13 @@ using UnityEngine;
 
 namespace StudioAssistPlugin
 {
-    [BepInPlugin("io.github.yuemenglong.assist", "StudioAssistPlugin", "1.0.0.0")]
-    public class StudioAssistPlugin : BaseUnityPlugin
+    [BepInPlugin("io.github.yuemenglong.assist.fk", "StudioAssistFKPlugin", "1.0.0.0")]
+    public class StudioAssistFKPlugin : BaseUnityPlugin
     {
         // Awake is called once when both the game and the plug-in are loaded
         void Awake()
         {
-            UnityEngine.Debug.Log("YML Hello, world!");
+            Tracer.Log("YML Hello, world!");
         }
 
 
@@ -24,8 +24,7 @@ namespace StudioAssistPlugin
         {
             try
             {
-                // Rotate();
-               Move();
+                Rotate();
             }
             catch (Exception e)
             {
@@ -35,54 +34,6 @@ namespace StudioAssistPlugin
         public static Camera MainCamera()
         {
             return Context.Studio().cameraCtrl.mainCmaera;
-        }
-
-        private void Move(Vector3 delta)
-        {
-//            var go = Context.GuideObjectManager().selectObject;
-            //            if (go == null || !go.enablePos)
-            //            {
-            //                return;
-            //            }
-
-            Camera camera = MainCamera();
-            if (camera == null)
-            {
-                return;
-            }
-
-            Vector3 vector31 = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, Input.mousePosition.z);
-            Ray ray = camera.ScreenPointToRay(vector31);
-            ray.direction = new Vector3(ray.direction.x, 0, ray.direction.z);
-            Vector3 vector32 = ray.direction * -1 * delta.z;
-            ray.direction = Quaternion.LookRotation(ray.direction) * Vector3.right;
-            Vector3 vector33 = vector32 + ray.direction * -1 * delta.x;
-            vector33.y = delta.y;
-            delta = vector33;
-            delta = delta * 2.0f;
-            Context.GuideObjectManager().selectObjects.Foreach(go =>
-            {
-                if (go.enablePos && go.IsLimb())
-                {
-                    go.Move(delta);
-                }
-            });
-        }
-
-        private void Move()
-        {
-            Vector2 sub = GetMousePos() - lastMousePos;
-            if (Input.GetKey(KeyCode.G))
-            {
-                Vector3 delta = new Vector3(-sub.x, 0, -sub.y);
-                Move(delta);
-            }
-            else if (Input.GetKey(KeyCode.H))
-            {
-                Vector3 delta = new Vector3(0, sub.y, 0);
-                Move(delta);
-            }
-            lastMousePos = GetMousePos();
         }
 
         private void Rotate()
