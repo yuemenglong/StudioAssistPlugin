dad9ac9ba059e5d459d5af184205c516b9686010
yuemenglong <yuemenglong@126.com>
2020-07-14 06:48:01 +0800

    add: 随相机移动，有瑕疵

--- StudioAssistPlugin/StudioAssistLimbLockPlugin.cs
+++ StudioAssistPlugin/StudioAssistLimbLockPlugin.cs
@@ -24,7 +24,7 @@ namespace StudioAssistPlugin
             public Quaternion Rot;
         }
 
-        private static List<FkChara> _headLocks = new List<FkChara>();
+        private static List<LockRecord> _headLocks = new List<LockRecord>();
 
         private static List<LockRecord> _lockRecord = new List<LockRecord>();
 
@@ -45,9 +45,9 @@ namespace StudioAssistPlugin
             {
                 GUIX.Label(r.Bone.GuideObject.transformTarget.name, 12);
             });
-            _headLocks.ForEach(chara =>
+            _headLocks.ForEach(r =>
             {
-                GUIX.Label(chara.Head.GuideObject.transformTarget.name, 12);
+                GUIX.Label(r.Bone.GuideObject.transformTarget.name, 12);
             });
         }
 
@@ -97,7 +97,20 @@ namespace StudioAssistPlugin
                 }
                 else if (go != null && (go.IsHead()))
                 {
-                    _headLocks.Add(FkCharaMgr.BuildChara(go));
+                    var r = new LockRecord();
+                    var bone = FkCharaMgr.BuildChara(go).Head;
+                    r.Bone = bone;
+                    r.Pos = bone.Transform.position;
+                    var cv = Context.MainCamera().transform.position - bone.Transform.position;
+                    if (Input.GetKey(KeyCode.LeftControl))
+                    {
+                        r.Rot = Quaternion.FromToRotation(cv, bone.GuideObject.transformTarget.forward);
+                    }
+                    else
+                    {
+                        r.Rot = Quaternion.FromToRotation(cv, cv);
+                    }
+                    _headLocks.Add(r);
                 }
             }
             else if (Input.GetKeyDown(KeyCode.T))
@@ -116,20 +129,28 @@ namespace StudioAssistPlugin
                     r.Bone.TurnTo(r.Rot);
                 }
             });
-            _headLocks.ForEach(chara =>
+            _headLocks.ForEach(r =>
             {
-                var go = new GameObject();
-                var target = Context.MainCamera().transform.position - chara.Head.Transform.position;
-                var v1 = go.transform.forward;
-                var v2 = new Vector3(target.x, 0, target.z);
-                var axis = Vector3.Cross(v1, v2);
-                var angel = Vector3.Angle(v1, v2);
-                go.transform.RotateAround(go.transform.position, axis, angel);
-                axis = Vector3.Cross(v2, target);
-                angel = Vector3.Angle(v2, target);
-                go.transform.RotateAround(go.transform.position, axis, angel);
-                chara.Head.TurnTo(go.transform.rotation);
-                GameObject.Destroy(go);
+                var bone = r.Bone;
+                var target = Context.MainCamera().transform.position - bone.Transform.position;
+                var from = new Vector3(0, 0, 1);
+                var p1 = Quaternion.FromToRotation(from, target);
+                var p2 = r.Rot;
+                bone.TurnTo(p2 * p1);
+
+                // var bone = r.Bone;
+                // var go = new GameObject();
+                // var target = Context.MainCamera().transform.position - bone.Transform.position;
+                // var v1 = go.transform.forward;
+                // var v2 = new Vector3(target.x, 0, target.z);
+                // var axis = Vector3.Cross(v1, v2);
+                // var angel = Vector3.Angle(v1, v2);
+                // go.transform.RotateAround(go.transform.position, axis, angel);
+                // axis = Vector3.Cross(v2, target);
+                // angel = Vector3.Angle(v2, target);
+                // go.transform.RotateAround(go.transform.position, axis, angel);
+                // bone.TurnTo(go.transform.rotation);
+                // GameObject.Destroy(go);
             });
         }
     }
